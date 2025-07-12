fcaa843948e6ef321cae42b8dc52ced781732b79
jay0dyer <jay0dyer@gmail.com>
2025-07-12 20:32:09 +0100

    added fundraising and learned how git worked

---
+++ fun.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("background").style.backgroundImage= "url('images/switchbutton/click.gif')";
+    setTimeout( function() { window.location = "https://www.justgiving.com/page/jay0dyer-switchboard?utm_medium=FR&utm_source=CL" }, 1100 );
+}
